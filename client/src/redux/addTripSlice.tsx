a6119aaeaf2251736fb04162f0969ed15b65a174
proc015 <proc015@gmail.com>
2023-11-27 17:00:47 +0100

    feat: connected front and backend

--- client/src/redux/addTripSlice.tsx
+++ client/src/redux/addTripSlice.tsx
@@ -1,25 +1,20 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit"
-import { RootState } from "../app/store"
-import { Trip } from "../types/Trip"
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from '../app/store';
+import { Trip } from '../types/Trip';
 
- const initialState: Trip[] = [];
-
+const initialState: Trip[] = [];
 
 export const addTripSlice = createSlice({
-    name: "trips", 
-    initialState, 
-    // The `reducers` field lets us define reducers and generate associated actions
+  name: 'trips',
+  initialState,
+  // The `reducers` field lets us define reducers and generate associated actions
   reducers: {
-    
     addTrip: (state, action: PayloadAction<Trip>) => {
-        state.push(action.payload); 
-    
+      state.push(action.payload);
     },
-    }, 
-})
+  },
+});
 
-export const { addTrip } = addTripSlice.actions
+export const { addTrip } = addTripSlice.actions;
 
-
-export default addTripSlice.reducer 
-
+export default addTripSlice.reducer;
