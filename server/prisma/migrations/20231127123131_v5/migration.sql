f5e9c5aa2a84b0da71697eb215034cd68d864d91
rogermarco <marcrogers90@gmail.com>
2023-11-27 13:41:11 +0100

    feat: update schema

---
+++ server/prisma/migrations/20231127123131_v5/migration.sql
@@ -0,0 +1,23 @@
+-- DropIndex
+DROP INDEX "Activity_tripId_key";
+
+-- DropIndex
+DROP INDEX "Comment_activityId_key";
+
+-- DropIndex
+DROP INDEX "Comment_authorId_key";
+
+-- DropIndex
+DROP INDEX "Comment_tripId_key";
+
+-- DropIndex
+DROP INDEX "Tag_activityId_key";
+
+-- DropIndex
+DROP INDEX "Trip_userId_key";
+
+-- DropIndex
+DROP INDEX "User_activityId_key";
+
+-- DropIndex
+DROP INDEX "User_tripId_key";
