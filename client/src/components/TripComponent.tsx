dc793d76e3da47d936c10eb75b91e6ebab947aff
proc015 <proc015@gmail.com>
2023-11-28 12:53:46 +0100

    feat: get trips working for all trips from postman mock server

---
+++ client/src/components/TripComponent.tsx
@@ -0,0 +1,21 @@
+import { Trip } from '../types/Trip';
+
+interface TripProps {
+  trip: Trip; 
+}
+
+const TripComponent = ({ trip }: TripProps) => {
+ 
+ console.log('trip component', trip)
+  return (
+    <div>
+      <p> {trip.trip_name}</p>
+      <p> {trip.start_loc}</p>
+      <p> {trip.destination}</p>
+      <p> {trip.start_date}</p>
+      <p> {trip.end_date}</p>
+    </div>
+  );
+};
+
+export default TripComponent;
