55bd209b72e0db02927c7d0b89746548711e43e6
sal-car <sally.carlund@gmail.com>
2023-11-28 18:42:18 +0100

    fix: restored previous commits

--- client/src/components/maps/activityMap.tsx
+++ client/src/components/maps/activityMap.tsx
@@ -1,22 +1,37 @@
-export const ActivityMap = () => {
-    // Markers will take either an address with correct format (e.g. '+' instead of whitespace) OR lat-lang nums
-    const options = {
-        markers: [],
-        mapId: '',
-        size: ''
-    }
+import { Autocompletion } from './autocompletion';
+import { useState } from 'react';
 
+type Props = {
+  setAddress: (address: string) => void;
+  location: string;
+};
+
+export const ActivityMap = ({ setAddress, location }: Props) => {
+  // Markers will take either an address with correct format (e.g. '+' instead of whitespace) OR lat-lang nums
+  const options = {
+    markers: [],
+    mapId: '',
+    size: '',
+  };
 
   return (
     <>
-    <div className="div" style={{height: "290px", overflow: "hidden"}}>
-      <img
-        src={
-          'https://maps.googleapis.com/maps/api/staticmap?&markers=Landala,Gothenburg|Majorna,Gothenburg|Berg,Sweden&size=300x300&key=AIzaSyA-Hi2FgH2KdyCeKTUNCy4BcExpre_suew&style=feature:poi|element:labels|visibility:off'
-        }
-      ></img>
-
-    </div>
+      <Autocompletion setAddress={setAddress}></Autocompletion>
+      <div
+        style={{
+          height: '200px',
+          overflow: 'hidden',
+          borderRadius: '15px',
+          width: 'fit-content',
+        }}
+      >
+        <img
+          style={{ borderRadius: '15px', height: '110%', width: '100%' }}
+          src={
+            'https://maps.googleapis.com/maps/api/staticmap?&markers=Landala,Gothenburg|Majorna,Gothenburg|Tynnered,Gothenburg&size=300x300&key=AIzaSyA-Hi2FgH2KdyCeKTUNCy4BcExpre_suew&style=feature:poi|element:labels|visibility:off'
+          }
+        ></img>
+      </div>
     </>
   );
 };
