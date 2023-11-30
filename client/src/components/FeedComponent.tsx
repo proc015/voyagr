b6c04f40fd28b9ad748a72efb93a8461c373a9a7
sal-car <sally.carlund@gmail.com>
2023-11-30 12:39:06 +0100

    chore: dev setup

--- client/src/components/FeedComponent.tsx
+++ client/src/components/FeedComponent.tsx
@@ -1,13 +1,16 @@
-import { TripFeed } from '../redux/fetchUserFeedSlice'
+import { TripFeed } from '../redux/fetchUserFeedSlice';
+import { Buttonbar } from './Buttonbar';
+import { Profilebar } from './Profilebar';
+import { Tripbox } from './Tripbox';
 
 interface FeedTripProp {
   feedTrip: TripFeed;
+  picture: string;
 }
 
-const FeedComponent = ({ feedTrip }: FeedTripProp) => {
-  
-    console.log('feedTrip', feedTrip);
-    console.log('feedTrip activity', feedTrip.activities);
+const FeedComponent = ({ feedTrip, picture }: FeedTripProp) => {
+  console.log('feedTrip', feedTrip);
+  console.log('feedTrip activity', feedTrip.activities);
 
   if (!feedTrip) {
     return null;
@@ -15,13 +18,24 @@ const FeedComponent = ({ feedTrip }: FeedTripProp) => {
 
   return (
     <div>
-      <h1>{feedTrip.trip_name}</h1>
+      {/* <Profilebar /> */}
+      <Tripbox
+        picture={picture}
+        name={feedTrip.trip_name}
+        startLocation={feedTrip.start_lat_lon}
+        destination={feedTrip.dest_lat_lon}
+        startDate={feedTrip.start_date}
+        endDate={feedTrip.end_date}
+        activities={feedTrip.activities}
+      />
+      {/* <Buttonbar /> */}
+      {/* <h1>{feedTrip.trip_name}</h1>
       <p>Start Location: {feedTrip.start_loc}</p>
       <p>Destination: {feedTrip.destination}</p>
       <p>Start Date: {feedTrip.start_date}</p>
-      <p>End Date: {feedTrip.end_date}</p>
+      <p>End Date: {feedTrip.end_date}</p> */}
 
-      {feedTrip.activities &&
+      {/* {feedTrip.activities &&
         feedTrip.activities.map((feedActivity) => (
           <div key={feedActivity.activity_id}>
             <h3> Activity: {feedActivity.activity_name}</h3>
@@ -29,7 +43,7 @@ const FeedComponent = ({ feedTrip }: FeedTripProp) => {
             <p>Location: {feedActivity.location}</p>
             <p>Type: {feedActivity.type}</p>
           </div>
-        ))}
+        ))} */}
     </div>
   );
 };
