97bc461b48feacba1e63a8f625378c0143dc1880
proc015 <proc015@gmail.com>
2023-11-30 12:13:12 +0100

    feat: WIP login

---
+++ client/src/components/Login/Login.tsx
@@ -0,0 +1,38 @@
+
+
+const Login = () => {
+  
+const handleSubmit = () => {
+
+}
+
+const handleChange = () => {
+
+}
+    
+  
+return (
+    <section>
+        <h3>Login</h3>
+        <form className="login-form" onSubmit={handleSubmit}>
+            <input 
+                type="text"
+                placeholder="insert email..."
+                name="email"
+                value={}
+                onChange={handleChange}
+            />
+            <input 
+                type="text"
+                placeholder="insert email..."
+                name="email"
+                value={}
+                onChange={handleChange}
+            />
+        </form>
+      
+    </section>
+  )
+}
+
+export default Login
