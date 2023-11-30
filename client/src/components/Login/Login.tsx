305d1427f58c9910a58b538ee4fd9fd733e6ebf7
proc015 <proc015@gmail.com>
2023-11-30 16:19:23 +0100

    feat:update changes

--- client/src/components/Login/Login.tsx
+++ client/src/components/Login/Login.tsx
@@ -1,13 +1,31 @@
+import { useState, ChangeEvent, FormEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { postLogin } from '../../services/loginService';
 
+const initalState = {
+  email: '',
+  password: '',
+};
 
 const Login = () => {
-  
-const handleSubmit = () => {
+  const navigate = useNavigate();
+  const [loginState, setLoginState] = useState(initalState);
 
-}
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setLoginState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
-const handleChange = () => {
+  const handleLogin = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const { email, password } = loginState;
+    const user = { email, password };
+    // console.log('user', user);
 
+<<<<<<< HEAD
 }
     
   
@@ -45,5 +63,51 @@ return (
     </section>
   )
 }
+=======
+    const response = await postLogin(user);
+ 
+    if (response.error) {
+      alert(`${response.message}`);
+      setLoginState(initalState);
+    } else {
+      navigate('/profile');
+    }
+  };
+>>>>>>> 3c1921a (feat: added frontend login and connected sign up to backend)
 
-export default Login
+  const validateForm = () => {
+    return !loginState.email || !loginState.password;
+  };
+
+  return (
+    <form
+      onSubmit={handleLogin}
+      style={{ display: 'flex', justifyContent: 'center' }}
+    >
+      <div
+        id='parent-container'
+        style={{ display: 'flex', flexDirection: 'column', width: '70%' }}
+      >
+        <input
+          type='text'
+          placeholder='insert email...'
+          name='email'
+          required={true}
+          value={loginState.email}
+          onChange={handleChange}
+        />
+        <input
+          type='password'
+          placeholder='type password...'
+          name='password'
+          required={true}
+          value={loginState.password}
+          onChange={handleChange}
+        />
+        <input type='submit' value='Login' disabled={validateForm()} />
+      </div>
+    </form>
+  );
+};
+
+export default Login;
