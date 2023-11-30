20486cc03091aa9138cbf57a6d65969243f715c2
rogermarco <marcrogers90@gmail.com>
2023-11-30 11:50:16 +0100

    feat: add extra fields to signup, init pass_hash field in new DB migration

--- client/src/components/Signup.tsx
+++ client/src/components/Signup.tsx
@@ -3,6 +3,8 @@ import { useState } from 'react';
 function Signup() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
 
   return (
     // lol css
@@ -23,8 +25,22 @@ function Signup() {
           onChange={(e) => setPassword(e.target!.value)}
         />
 
-        <button type='submit' 
-        // onClick={handleClick}
+        <input
+          type='text'
+          placeholder='First name'
+          onChange={(e) => setFirstName(e.target!.value)}
+        />
+
+        <input
+          type='text'
+          placeholder='Last name'
+          onChange={(e) => setLastName(e.target!.value)}
+        />
+
+        <button
+          type='submit'
+          // onClick={handleClick}
+          // send these states to backend
         >
           Register
         </button>
