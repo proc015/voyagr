f9adcbb50ba1bf1fbc01a875c66ceadcfd3d27b1
rogermarco <marcrogers90@gmail.com>
2023-12-01 21:52:29 +0100

    feat: render search results on the screen and add placeholder profile image

--- client/src/components/SearchBar.tsx
+++ client/src/components/SearchBar.tsx
@@ -1,9 +1,16 @@
+import { useState } from 'react';
 import { searchUsers } from '../services/searchService';
+import noprofilepic from '../assets/images/noprofilepic.jpg'
 
 function SearchBar() {
+  const [searchResult, setSearchResult] = useState<any>([])
+
+  const cloudinaryUrl = 'https://res.cloudinary.com/dwskyhib9/image/upload/'
+
   const handleSearch = async (searchTerm: string) => {
     const response = await searchUsers(searchTerm);
-    console.log(response);
+    setSearchResult(response);
+    console.log(searchResult);
   };
 
   return (
@@ -13,6 +20,23 @@ function SearchBar() {
         placeholder='Search for users'
         onChange={(e) => handleSearch(e.target.value)}
       />
+      <div id='search results'>
+        {searchResult.map((result: any) => {
+          return (<div className='h-28 border-2 border-b-voyagrBlack my-1 flex'>
+                    {(result.display_pic_src) ?
+                    <img src={`${cloudinaryUrl}${result.display_pic_src}`}
+                    className='object-contain'
+                    />
+                    :
+                    <img src={`${noprofilepic}`}
+                    className='object-contain'
+                    />
+                    }
+                    <div>{result.display_name}</div>
+                  </div>
+          )
+        })}        
+      </div>
     </main>
   );
 }
