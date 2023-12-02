df1293ea73b7a367e95437a7c637d41a34fb1df5
rogermarco <marcrogers90@gmail.com>
2023-12-02 13:11:16 +0100

    feat: better search render handling, search bar styling

--- client/src/components/SearchBar.tsx
+++ client/src/components/SearchBar.tsx
@@ -1,41 +1,61 @@
 import { useState } from 'react';
 import { searchUsers } from '../services/searchService';
-import noprofilepic from '../assets/images/noprofilepic.jpg'
+import noprofilepic from '../assets/images/noprofilepic.jpg';
+import searchIcon from '../assets/icons/search-icon.svg';
 
 function SearchBar() {
-  const [searchResult, setSearchResult] = useState<any>([])
+  const [searchResult, setSearchResult] = useState<any>([]);
 
-  const cloudinaryUrl = 'https://res.cloudinary.com/dwskyhib9/image/upload/'
+  const cloudinaryUrl = 'https://res.cloudinary.com/dwskyhib9/image/upload/';
 
   const handleSearch = async (searchTerm: string) => {
-    const response = await searchUsers(searchTerm);
-    setSearchResult(response);
-    console.log(searchResult);
+    if (searchTerm) {
+      const response = await searchUsers(searchTerm);
+      setSearchResult(response);
+    } else {
+      setSearchResult('');
+    }
   };
 
   return (
     <main>
-      <input
-        type='search'
-        placeholder='Search for users'
-        onChange={(e) => handleSearch(e.target.value)}
-      />
+      <div className='m-auto h-10 mt-5 w-[90%] rounded-xl bg-voyagrLightGrey bg-opacity-20 flex'>
+        <img src={`${searchIcon}`}
+        className='h-5 mt-2.5 pl-4 opacity-40' 
+        />
+        <input
+          type='search'
+          placeholder='Search'
+          className='w-full h-10 pl-3 font-didact rounded-xl text-voyagrBlack bg-voyagrLightGrey bg-opacity-0 text-lg focus:outline-none'
+          onChange={(e) => handleSearch(e.target.value)}
+        />
+      </div>
       <div id='search results'>
-        {searchResult.map((result: any) => {
-          return (<div className='h-28 border-2 border-b-voyagrBlack my-1 flex'>
-                    {(result.display_pic_src) ?
-                    <img src={`${cloudinaryUrl}${result.display_pic_src}`}
-                    className='object-contain'
-                    />
-                    :
-                    <img src={`${noprofilepic}`}
-                    className='object-contain'
-                    />
-                    }
-                    <div>{result.display_name}</div>
-                  </div>
-          )
-        })}        
+        {/* ONLY RENDER SEARCH RESULTS IF STATE IS DEFINED */}
+        {searchResult.length > 0 &&
+          searchResult.map((result: any) => {
+            return (
+              <div className='h-20 my-7 flex'>
+                {/* RENDER DEFAULT PIC IF NONE SET */}
+                {result.display_pic_src ? (
+                  <img
+                    src={`${cloudinaryUrl}${result.display_pic_src}`}
+                    className='rounded-full w-20 h-auto'
+                  />
+                ) : (
+                  <img
+                    src={`${noprofilepic}`}
+                    className='rounded-full w-20 h-auto'
+                  />
+                )}
+                <div className='w-full flex'>
+                  <p className='pl-5 flex items-center font-didact text-lg'>
+                    {result.display_name}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
       </div>
     </main>
   );
