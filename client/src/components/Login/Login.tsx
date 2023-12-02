f14f19f0f480824e726d2051d7a9741caec0544a
rogermarco <marcrogers90@gmail.com>
2023-12-02 15:23:45 +0100

    feat: init loginPage styling

--- client/src/components/Login/Login.tsx
+++ client/src/components/Login/Login.tsx
@@ -64,7 +64,8 @@ const Login = () => {
           value={loginState.password}
           onChange={handleChange}
         />
-        <input type='submit' value='Login' disabled={validateForm()} />
+        <input type='submit' value='Login' disabled={validateForm()} className='landing-btn'/>
+        <input type='button' value='Sign up' disabled={validateForm()} className='landing-btn'/>
       </div>
     </form>
   );
