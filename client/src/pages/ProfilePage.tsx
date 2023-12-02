0c7bb76a71b6d247c0b0d0c56eab6e55c99ce9fe
Nils Wagner <nils.wagner@hamburg.de>
2023-12-02 12:06:43 +0100

    feat: about 1/3 of the PP styling done

--- client/src/pages/ProfilePage.tsx
+++ client/src/pages/ProfilePage.tsx
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom';
 import ProfileMe from '../components/Profile/ProfileMe';
+import TripProfile from '../components/Trip/TripProfile';
 
 function ProfilePage() {
   return (
     <>
       <ProfileMe />
+      <TripProfile />
       This is the ProfilePage
     </>
   );
