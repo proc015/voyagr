2ee928091b58d3e91f1e7a64f216ba27ea173c14
sal-car <sally.carlund@gmail.com>
2023-12-05 18:01:00 +0100

    feat: styling navbar

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -3,8 +3,14 @@ import './NavBar.css';
 import travelIcon from '../../assets/icons/button-active.svg';
 import travelIconnoBackground from '../../assets/icons/traveller.svg';
 import { useMatch } from 'react-router-dom';
-import searchIcon from '../../assets/icons/search-icon.svg';
+import searchIcon from '../../assets/icons/search-black.svg';
+// import activeSearch from '../../assets/icons/search-orange.svg';
 import { Header } from './header';
+import homeIcon from '../../assets/icons/home-black.svg';
+import profileIcon from '../../assets/icons/profile-black.svg';
+import activeSearch from '../../assets/icons/search-orange.svg';
+import activeHome from '../../assets/icons/home-orange.svg';
+import activeProfile from '../../assets/icons/profile-orange.svg';
 
 type Props = {
   page: string;
@@ -12,19 +18,22 @@ type Props = {
 
 const NavBar = ({ page }: Props) => {
   const addTripMatch = useMatch('/addtrip');
+  const feedMatch = useMatch('/feed');
+  const searchMatch = useMatch('/search');
+  const profileMatch = useMatch('/profile');
 
   return (
     <>
       <Header page={page} />
 
-      <div className='navbar'>
+      <div className='navbar px-5'>
         <NavLink
           to='/feed'
           className={({ isActive }) =>
             isActive ? 'nav-link nav-link-active' : 'nav-link'
           }
         >
-          Feed
+          {feedMatch ? <img src={activeHome} /> : <img src={homeIcon} />}
         </NavLink>
         <NavLink
           to='/search'
@@ -32,11 +41,15 @@ const NavBar = ({ page }: Props) => {
             isActive ? 'nav-link nav-link-active' : 'nav-link'
           }
         >
-          <img className='h-6' src={searchIcon} />
+          {searchMatch ? <img src={activeSearch} /> : <img src={searchIcon} />}
         </NavLink>
         <div className='nav-link-container'>
           {addTripMatch ? (
-            <img src={travelIcon} alt='travelIcon' className='active-icon' />
+            <img
+              src={travelIcon}
+              alt='travelIcon'
+              className='active-icon mb-2'
+            />
           ) : null}
           <NavLink
             to='/addtrip'
@@ -61,7 +74,11 @@ const NavBar = ({ page }: Props) => {
             isActive ? 'nav-link nav-link-active' : 'nav-link'
           }
         >
-          Profile
+          {profileMatch ? (
+            <img src={activeProfile} />
+          ) : (
+            <img src={profileIcon} />
+          )}{' '}
         </NavLink>
       </div>
     </>
