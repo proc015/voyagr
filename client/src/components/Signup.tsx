90e7bd4f69090ebc22217e1bbbd26b0d5febfaf8
rogermarco <marcrogers90@gmail.com>
2023-12-05 16:06:30 +0100

    feat: BESTCOMMITEVER this is the crispiest signup component ever made

--- client/src/components/Signup.tsx
+++ client/src/components/Signup.tsx
@@ -1,57 +1,77 @@
-import { useState, FormEvent, ChangeEvent } from 'react';
+import { useState, FormEvent, ChangeEvent, MouseEvent } from 'react';
 import { createUser } from '../services/signupService';
-import { RegisterUser } from '../types/RegisterUser';
 import { useNavigate } from 'react-router-dom';
 import traveller from '../assets/icons/traveller.svg';
 import backButton from '../assets/icons/chevron-left.svg';
 import { useDispatch } from 'react-redux';
-import SignupDetails from './SignupDetails';
 import { Tab } from '@headlessui/react';
+import { setUser } from '../redux/userSlice';
+import { uploadPhoto } from '../services/apiService';
 
-const initialState = {
+export const initialSignupState = {
   email: '',
   password: '',
   firstName: '',
   lastName: '',
   displayName: '',
+  displayPicSrc: '',
 };
 
-export interface SignupProps {
-  setOpenSignup: Function;
-}
-
-function Signup({ setOpenSignup }: SignupProps) {
+function Signup({ setOpenSignup }: { setOpenSignup: Function }) {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [signupState, setSignupState] = useState(initialState);
-  const [userDetails, setUserDetails] = useState(false);
+  const [signupState, setSignupState] = useState(initialSignupState);
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
+  const handlePhotoUpload = (event: ChangeEvent<HTMLInputElement>) => {
+    event.stopPropagation();
+    const filename = event.target.files![0].name;
+    console.log('FILENAME ', filename);
+
+    setSignupState((prevState) => ({
+      ...prevState,
+      [event.target.name]: filename,
+    }));
+    uploadPhoto(event.target.files);
+  };
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
     setSignupState((prevState) => ({
       ...prevState,
       [name]: value,
-    }))
+    }));
   };
 
-  const handleCreateUser = async (event: FormEvent<HTMLFormElement>) => {
+  const handleCreateUser = async (event: MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
-    const { email, password, firstName, lastName, displayName } = signupState;
-    const user = { email, password, firstName, lastName, displayName }
+    const { email, password, firstName, lastName, displayName, displayPicSrc } =
+      signupState;
+    const user = {
+      email,
+      password,
+      firstName,
+      lastName,
+      displayName,
+      displayPicSrc,
+    };
+    console.log('USER', user);
 
     const response = await createUser(user);
 
     if (response.error) {
       alert(`${response.message}`);
+      setSignupState(initialSignupState);
     } else {
+      console.log('LOGIN RESPONSE', response);
+      dispatch(setUser(response.user_id));
       navigate('/profile');
     }
   };
 
   return (
     <main className='bg-voyagrWhite h-[600px] relative z-10 rounded-2xl'>
-      <Tab.Group>
+      <Tab.Group selectedIndex={selectedIndex} onChange={setSelectedIndex}>
         <div id='top-bar' className='flex h-14 border-b border-voyagrBlack'>
           <img
             src={`${backButton}`}
@@ -59,73 +79,115 @@ function Signup({ setOpenSignup }: SignupProps) {
             onClick={() => setOpenSignup(false)}
           />
           <Tab.List className='font-noto my-auto w-full text-center mr-8 space-x-5'>
-            <Tab className='ui-selected:border-b-2 ui-selected:border-voyagrBlack'>sign up</Tab>
-            <Tab className='ui-selected:border-b-2 ui-selected:border-voyagrBlack'>continue</Tab>
+            <Tab
+              className='ui-selected:border-b-2 ui-selected:border-voyagrRed'
+              onClick={() => setSelectedIndex(0)}
+            >
+              sign up
+            </Tab>
+            <Tab
+              className='ui-selected:border-b-2 ui-selected:border-voyagrRed'
+              onClick={() => setSelectedIndex(1)}
+            >
+              continue
+            </Tab>
           </Tab.List>
         </div>
 
-      <Tab.Panels>
-        <Tab.Panel>
-      <img src={`${traveller}`} className='h-24 w-auto m-auto my-8' />
-      <form className='flex justify-center'>
-        <div
-          id='parent-container'
-          className='flex flex-col w-[90%] items-center'
-        >
-          <input
-            type='email'
-            name='email'
-            value={signupState.email}
-            required={true}
-            placeholder='Email'
-            onChange={handleChange}
-            className='login-box'
-          />
+        <Tab.Panels>
+          <Tab.Panel>
+            <img src={`${traveller}`} className='h-24 w-auto m-auto my-8' />
+            <form className='flex justify-center' 
+            // onSubmit={handleCreateUser}
+            >
+              <div
+                id='parent-container'
+                className='flex flex-col w-[90%] items-center'
+              >
+                <input
+                  type='email'
+                  name='email'
+                  value={signupState.email}
+                  required={true}
+                  placeholder='Email'
+                  onChange={handleChange}
+                  className='login-box'
+                />
 
-          <input
-            type='password'
-            name='password'
-            value={signupState.password}
-            required={true}
-            placeholder='Password'
-            onChange={handleChange}
-            className='login-box'
-          />
-          <div id='nameContainer' className='flex space-x-7'>
-            <input
-              type='text'
-              name='firstName'
-              value={signupState.firstName}
-              required={true}
-              placeholder='First name'
-              onChange={handleChange}
-              className='login-box'
-            />
+                <input
+                  type='password'
+                  name='password'
+                  value={signupState.password}
+                  required={true}
+                  placeholder='Password'
+                  onChange={handleChange}
+                  className='login-box'
+                />
+                <div id='nameContainer' className='flex space-x-7'>
+                  <input
+                    type='text'
+                    name='firstName'
+                    value={signupState.firstName}
+                    required={true}
+                    placeholder='First name'
+                    onChange={handleChange}
+                    className='login-box'
+                  />
 
-            <input
-              type='text'
-              name='lastName'
-              required={true}
-              placeholder='Last name'
-              onChange={handleChange}
-              className='login-box'
-            />
-          </div>
-            <button className='landing-btn'>Continue</button>
-        </div>
-      </form>
-        </Tab.Panel>
+                  <input
+                    type='text'
+                    name='lastName'
+                    value={signupState.lastName}
+                    required={true}
+                    placeholder='Last name'
+                    onChange={handleChange}
+                    className='login-box'
+                  />
+                </div>
+                <div
+                  className='landing-btn'
+                  onClick={() => setSelectedIndex(1)}
+                >
+                  Continue
+                </div>
+              </div>
+            </form>
+          </Tab.Panel>
 
-        <Tab.Panel>
-          <SignupDetails />
-        </Tab.Panel>
-
-      </Tab.Panels>
+          <Tab.Panel>
+            <main className='bg-voyagrWhite h-[600px] relative z-10 rounded-2xl'>
+              <img src={`${traveller}`} className='h-24 w-auto m-auto my-8' />
+              <form className='flex justify-center'>
+                <div
+                  id='parent-container'
+                  className='flex flex-col w-[90%] items-center'
+                >
+                  <input
+                    type='file'
+                    accept='image/png, image/jpeg'
+                    name='displayPicSrc'
+                    required={true}
+                    onChange={(e) => handlePhotoUpload(e)}
+                    className='image-upload'
+                  />
+                  <input
+                    type='text'
+                    name='displayName'
+                    value={signupState.displayName}
+                    required={true}
+                    placeholder='Display name'
+                    onChange={handleChange}
+                    className='login-box'
+                  />
+                  <button className='landing-btn' onClick={(e) => handleCreateUser(e)}>Finish</button>
+                </div>
+              </form>
+            </main>
+          </Tab.Panel>
+        </Tab.Panels>
       </Tab.Group>
     </main>
   );
 }
-
-//           
 
 export default Signup;
--- client/src/components/SignupDetails.tsx
+++ client/src/components/SignupDetails.tsx
@@ -1,28 +1,39 @@
+
 import traveller from '../assets/icons/traveller.svg';
 
-function SignupDetails () {
+function SignupDetails({ signupState, handleChange, handlePhotoUpload }) {
 
-  return ( 
+  return (
     <main className='bg-voyagrWhite h-[600px] relative z-10 rounded-2xl'>
-      <img src={`${traveller}`} 
-      className='h-24 w-auto m-auto my-8' 
-      />
+      <img src={`${traveller}`} className='h-24 w-auto m-auto my-8' />
       <form className='flex justify-center'>
-        <div id='parent-container' className='flex flex-col w-[90%] items-center'>
+        <div
+          id='parent-container'
+          className='flex flex-col w-[90%] items-center'
+        >
+          <input
+            type='file'
+            accept='image/png, image/jpeg'
+            name='displayPicSrc'
+            required={true}
+            onChange={(e) => handlePhotoUpload(e)}
+            // className='block w-full text-lg border rounded-xl cursor-pointer h-12'
+            className='image-upload'
+          />
           <input
             type='text'
             name='displayName'
-            // value={signupState.displayName}
+            value={signupState.displayName}
             required={true}
             placeholder='Display name'
-            // onChange={handleChange}
-            className='login-box'
+            onChange={handleChange}
+            className='login-box rounded'
           />
           <button className='landing-btn'>Finish</button>
         </div>
       </form>
     </main>
-   );
+  );
 }
 
 export default SignupDetails;
