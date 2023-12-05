01a33f948a9ccc2d4e4c1674d873f0bd9fcc982d
Nils Wagner <nils.wagner@hamburg.de>
2023-12-05 17:45:17 +0100

    fix: now publish button working

--- client/src/components/Publish.tsx
+++ client/src/components/Publish.tsx
@@ -2,28 +2,44 @@ import { MouseEventHandler } from 'react';
 import { publishTrip } from '../services/apiService';
 import { useSelector } from 'react-redux';
 import { RootState } from '../app/store';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Publish = () => {
-  const trip_id: number = useSelector(
-    (state: RootState) => state.tripid.currentTrip
+  const trip_id = useSelector(
+    (state: RootState) => state.lastTrip.lastTrip.trip_id
   );
+  const navigate = useNavigate();
 
   const handlePublish: MouseEventHandler<HTMLButtonElement> = (event) => {
     event.preventDefault();
     console.log('the button was clicked');
     publishTrip(trip_id);
+    navigate('/feed');
   };
 
+  const content =
+    trip_id === 0 || trip_id === null ? (
+      <div className='flex text-black py-[3px] px-[40px] rounded-full bg-voyagr mx-auto text-xl '>
+        <div className='flex text-voyagrLightGrey py-[3px] px-[40px] rounded-full bg-voyagr border-voyagrLightGrey border-[1px] font-noto mx-auto '>
+          <div>not possible yet 🖕</div>
+        </div>
+      </div>
+    ) : (
+      <div className='flex text-black py-[3px] px-[40px] rounded-full bg-voyagr mx-auto text-xl '>
+        <Link
+          to='/feed'
+          onClick={handlePublish}
+          className='flex text-voyagrBlue py-[3px] px-[40px] rounded-full bg-voyagr border-voyagrBlue border-[1px] font-noto mx-auto '
+        >
+          <div>End & Publish!</div>
+        </Link>
+      </div>
+    );
+
   return (
-    <div className='flex text-black py-[3px] px-[40px] rounded-full bg-voyagr mx-auto text-xl '>
-      <button
-        onClick={handlePublish}
-        className='flex text-voyagrBlue py-[3px] px-[40px] rounded-full bg-voyagr border-voyagrBlue border-[1px] font-noto mx-auto '
-      >
-        <Link to='/feed'>End & Publish!</Link>
-      </button>
-    </div>
+    <>
+      {content}
+    </>
   );
 };
 
